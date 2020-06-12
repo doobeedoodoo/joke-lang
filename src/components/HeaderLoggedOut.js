8fc25daf5b976abfe9e1ab720cacc44e99a1c3d9
Mac Sta. Maria <mac.stamaria@gmail.com>
2020-06-12 12:56:31 +0800

    Added functionality for logging in and out.

---
+++ src/components/HeaderLoggedOut.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react"
+import { Auth } from "aws-amplify"
+
+function HeaderLoggedOut(props) {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const user = await Auth.signIn(username, password)
+      console.log("Login successful." + JSON.stringify(user))
+      props.setLoggedIn(true)
+    } catch (e) {
+      console.log("There was an error logging in.")
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
+      <div className="row align-items-center">
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input onChange={e => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+        </div>
+        <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
+          <input onChange={e => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+        </div>
+        <div className="col-md-auto">
+          <button className="btn btn-success btn-sm">Sign In</button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default HeaderLoggedOut
